86c30a8125285f5ec288a2c4bc0eec0f4924a09a
FreshWeb901028 <44171844+FreshWeb901028@users.noreply.github.com>
2019-08-08 23:02:46 +0800

    Update Dashboard.js

--- react-admin-master/examples/tutorial/src/Dashboard.js
+++ react-admin-master/examples/tutorial/src/Dashboard.js
@@ -7,7 +7,15 @@ export default () => (
     <Card>
         <CardHeader title="Welcome to the administration" />
         <CardContent>Lorem ipsum sic dolor amet...</CardContent>
-        <CardHeader title="My Card" />
-        <CardContent>This is my card.</CardContent>
+        <CardHeader title="My Card1" />
+        <CardContent>This is my card1.</CardContent>
+        <CardHeader title="My Card2" />
+        <CardContent>This is my card2.</CardContent>
+        <CardHeader title="My Card3" />
+        <CardContent>This is my card3.</CardContent>
+        <CardHeader title="My Card4" />
+        <CardContent>This is my card4.</CardContent>
+        <CardHeader title="My Card5" />
+        <CardContent>This is my card5.</CardContent>
     </Card>
 );
